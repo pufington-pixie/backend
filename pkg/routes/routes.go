600bd79e6d75d1cdbadcbd4a5ac01237563aecc4
pufinton-pixie <kg6092@srmist.edu.in>
2023-06-18 10:14:11 +0530

    .

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -18,105 +18,22 @@ import (
 func SetRoutes() {
 	r := chi.NewRouter()
 
-	// Routes for project information
-	/**
-	 * @swagger
-	 * /projects:
-	 *   get:
-	 *     summary: Get all projects
-	 *     tags:
-	 *       - Projects
-	 *     responses:
-	 *       200:
-	 *         description: Success
-	 */
+	
 	r.Get("/projects", controller.GetProject)
 
-	/**
-	 * @swagger
-	 * /projects:
-	 *   post:
-	 *     summary: Create a new project
-	 *     tags:
-	 *       - Projects
-	 *     responses:
-	 *       200:
-	 *         description: Success
-	 */
+	
 	r.Post("/projects", controller.InsertProject)
 
-	/**
-	 * @swagger
-	 * /projects/{id}:
-	 *   get:
-	 *     summary: Get a project by ID
-	 *     tags:
-	 *       - Projects
-	 *     parameters:
-	 *       - in: path
-	 *         name: id
-	 *         required: true
-	 *         description: ID of the project
-	 *         schema:
-	 *           type: integer
-	 *     responses:
-	 *       200:
-	 *         description: Success
-	 */
+	
 	r.Get("/projects/{id}", controller.GetProjectByID)
 
-	/**
-	 * @swagger
-	 * /projects/{id}:
-	 *   put:
-	 *     summary: Update a project by ID
-	 *     tags:
-	 *       - Projects
-	 *     parameters:
-	 *       - in: path
-	 *         name: id
-	 *         required: true
-	 *         description: ID of the project
-	 *         schema:
-	 *           type: integer
-	 *     responses:
-	 *       200:
-	 *         description: Success
-	 */
+	
 	r.Put("/projects/{id}", controller.UpdateProject)
 
-	/**
-	 * @swagger
-	 * /projects/{id}:
-	 *   delete:
-	 *     summary: Delete a project by ID
-	 *     tags:
-	 *       - Projects
-	 *     parameters:
-	 *       - in: path
-	 *         name: id
-	 *         required: true
-	 *         description: ID of the project
-	 *         schema:
-	 *           type: integer
-	 *     responses:
-	 *       200:
-	 *         description: Success
-	 */
+
 	r.Delete("/projects/{id}", controller.DeleteProject)
 
-	// Routes for FileUpload
-	/**
-	 * @swagger
-	 * /upload:
-	 *   post:
-	 *     summary: Upload a file
-	 *     tags:
-	 *       - FileUpload
-	 *     responses:
-	 *       200:
-	 *         description: Success
-	 */
+	
 	r.Post("/upload/{id}", controller.UploadHandler)
 
 	// Swagger UI route
